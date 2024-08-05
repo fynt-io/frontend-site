20fc99835192a56fea3f5f7e3465d01648aeacad
Bruno Formagio <45725210+brunoformagio@users.noreply.github.com>
2024-08-05 13:06:24 -0300

    sync (#16)
    * vars (#14)
    
    * feat: staging promotion workflow
    
    * feat: staging promotion workflow
    
    * feat: production promotion workflow set up
    
    * fix: promotion workflows - subdirectory addition
    
    * fix: changing network service paradigm to NodePort type
    
    * feat: production promotion workflow
    
    * feat: ingress and tls creation
    
    * feat: ingress and tls creation
    
    * feat: ingress and tls creation
    
    * feat: ingress and tls creation
    
    * feat: ingress and tls creation
    
    * fix identation
    
    * feat: ingress and tls creation
    
    * feat: ingress and tls creation
    
    * feat: ingress and tls creation
    
    * feat: ingress and tls creation
    
    * feat: ingress and tls creation
    
    * feat: ingress and tls creation
    
    * feat: ingress and tls creation
    
    * feat: ingress and tls creation
    
    * feat: ingress and tls creation
    
    * feat: ingress and tls creation
    
    * feat: ingress and tls creation
    
    * fix: site vars
    
    * fix: site vars
    
    * fix: site vars
    
    * fix: site vars
    
    * fix: site vars
    
    * fix: healthcheck
    
    * fix: vars in dockerfile
    
    * fix: vars in site dockerfile
    
    * fix: vars in site dockerfile
    
    ---------
    
    Co-authored-by: Matheus Ramos <matheus-ramos@debian-workspace.bren>
    Co-authored-by: Alice  Paixão <alicepaixao.thoughtworks@latam.com>
    
    * changing env key (#15)
    
    ---------
    
    Co-authored-by: matheus-rsouza <156213241+matheus-rsouza@users.noreply.github.com>
    Co-authored-by: Matheus Ramos <matheus-ramos@debian-workspace.bren>
    Co-authored-by: Alice  Paixão <alicepaixao.thoughtworks@latam.com>

--- app/api/apiRequester.tsx
+++ app/api/apiRequester.tsx
@@ -1,12 +1,13 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import { SendPlatformMessage } from '../Utils/Utils';
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 import Cookies from "js-cookie";
+import { GetEnvValue } from './env';
 
 export interface ApiResponse<T> {
   data: T;
   status: number;
 }
+
 
 async function request<T>(
   url: string,
@@ -15,7 +16,11 @@ async function request<T>(
   params?: any,
   contentType?: string,
 ): Promise<ApiResponse<T> | undefined> {
+
+  const BASE_URL = await GetEnvValue("NEXT_PUBLIC_API_URL");
+
   try {
+    // Inicializa headers como um objeto vazio, se não for fornecido
     const config: AxiosRequestConfig = {
       url: `${BASE_URL}${url}`,
       method,
@@ -26,6 +31,17 @@ async function request<T>(
         'Content-Type': contentType || 'application/json',
       },
     };
+    // Se `data` for uma instância de FormData, o Content-Type deve ser removido
+    if (data instanceof FormData) {
+      // Garantir que headers está definido
+      if (config.headers) {
+        delete config.headers['Content-Type'];
+      }
+    } else {
+      if (config.headers) {
+        config.headers['Content-Type'] = contentType || 'application/json';
+      }
+    }
 
     const response: AxiosResponse<T> = await axios(config);
     return { data: response.data, status: response.status };
@@ -41,10 +57,11 @@ async function request<T>(
     if (error.response && error.response.status !== 404 && url === "/faq/") {
       throw error.response ? SendPlatformMessage(error.response.data) : SendPlatformMessage(error);
     }
-    
+
     // Adicione um retorno no final para satisfazer o TypeScript
     return undefined;
   }
+  
 }
 
 export default request;
